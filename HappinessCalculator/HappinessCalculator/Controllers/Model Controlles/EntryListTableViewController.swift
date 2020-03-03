ffb31f87043ac3259d9035e4503dfa651d62fcdb
DanielMR <jadaniel54@gmal.com>
2020-03-03 12:10:08 -0700

    Added in Updating UI

--- HappinessCalculator/HappinessCalculator/Controllers/Model Controlles/EntryListTableViewController.swift
+++ HappinessCalculator/HappinessCalculator/Controllers/Model Controlles/EntryListTableViewController.swift
@@ -9,19 +9,20 @@
 import UIKit
 
 class EntryListTableViewController: UITableViewController {
-
+    
+    var averageHappiness: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return EntryController.entries.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "EntryCell", for: indexPath) as? EntryCellTableViewCell else
@@ -30,15 +31,28 @@ class EntryListTableViewController: UITableViewController {
         cell.setEntry(entry: entry, avergeHappiness: 0)
         
         cell.delegate = self
-
+        
         // Configure the cell...
-
+        
         return cell
     }
-}
+    
+    func updatAverageHappiness() {
+        var totalHappiness = 0
+        for entry in EntryController.entries {
+            if entry.isIncluded {
+                totalHappiness += entry.happiness
+            }
+        }
+        averageHappiness = totalHappiness / EntryController.entries.count
+    }
+}// End Of Class
 
 extension EntryListTableViewController: EntryTableViewCellDelegate {
     func switchToggledOnCell(cell: EntryCellTableViewCell) {
-        print("Cell was tapped")
+        guard let indexPath = tableView.indexPath(for: cell), let entry = cell.entry else {return}
+        EntryController.updateEntry(entry: entry)
+        updatAverageHappiness()
+        cell.updateUi(averageHappiness: averageHappiness)
     }
 }
